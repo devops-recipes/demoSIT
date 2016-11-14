af6d6fdac61b01fb8d6ad659bbc3cde76edadbf3
Avi Cavale <avi@shippable.com>
2016-11-15 05:37:19 +0900

    adding sits

---
+++ tests/core/1.js
@@ -0,0 +1,36 @@
+'use strict';
+
+var apiURL = 'http://' + process.env.API_URL;
+
+var superagent = require('superagent');
+var chai = require('chai');
+var expect = chai.expect;
+var should = require('should');
+
+describe('Core Test Suite',
+  function () {
+    it('Testing Index call to ' + apiURL,
+      function (done) {
+        superagent.get(apiURL).end(
+          function (err, res) {
+            (err === null).should.equal(true);
+            res.statusCode.should.equal(200);
+            done();
+          }
+        );
+      }
+    );
+    it('Testing Info call to ' + apiURL,
+      function (done) {
+        superagent.get(apiURL + 'info').end(
+          function (err, res) {
+            (err === null).should.equal(true);
+            res.statusCode.should.equal(200);
+            res.body.should.have.property('time');
+            done();
+          }
+        );
+      }
+    );
+  }
+);
